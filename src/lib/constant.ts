a533cac7a4a341695fec19bda10a9ea35e83a679
arham <148682737+Azan-Asim@users.noreply.github.com>
2026-04-27 10:57:04 +0500

    Update constant.ts

--- src/lib/constant.ts
+++ src/lib/constant.ts
@@ -1,3 +1,3 @@
-export const BASE_URL = "https://vendor.umazing.shop";
-// export const BASE_URL = "https://drm.devsinntechnologies.com";
+// export const BASE_URL = "https://vendor.umazing.shop";
+export const BASE_URL = "drm.devsinntechnologies.com";
 
