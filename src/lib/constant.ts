5c57b3f2618fd11f385aef9dc737b11adff92e43
Arham Sarwar <arhamsarwar786@gmail.com>
2026-08-16 19:52:08 +0500

    Update constant.ts

--- src/lib/constant.ts
+++ src/lib/constant.ts
@@ -1,4 +1,5 @@
-export const BASE_URL =
-  process.env.NEXT_PUBLIC_API_URL ||
-  process.env.NEXT_PUBLIC_BASE_URL ||
-  "http://localhost:4000";
+// export const BASE_URL =
+//   process.env.NEXT_PUBLIC_BASE_URL ||
+//   process.env.NEXT_PUBLIC_API_URL ||
+//   "https://vendor.umazing.shop";
+export const BASE_URL = "https://drm.devsinntechnologies.com";
