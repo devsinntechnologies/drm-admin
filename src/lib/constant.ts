2b0fe38e5bc7f07bff2cd68080a5b3ad26a24d1b
Azan-Asim <148682737+Azan-Asim@users.noreply.github.com>
2026-08-17 23:44:56 +0500

    Update constant.ts

--- src/lib/constant.ts
+++ src/lib/constant.ts
@@ -1,4 +1,5 @@
-export const BASE_URL =
-  process.env.NEXT_PUBLIC_BASE_URL ||
-  process.env.NEXT_PUBLIC_API_URL ||
-  "https://drm.devsinntechnologies.com";
+// export const BASE_URL =
+//   process.env.NEXT_PUBLIC_BASE_URL ||
+//   process.env.NEXT_PUBLIC_API_URL ||
+//   "https://drm.devsinntechnologies.com";
+export const BASE_URL = "https://vendor.umazing.shop";
